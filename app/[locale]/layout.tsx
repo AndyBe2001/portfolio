9b1b6bfa76812ebca180285f58c45e0b7f665c8a
AndyBe2001 <andybe034@gmail.com>
2024-05-13 16:06:30 +0800

    feat(static page): add static page generator

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -1,6 +1,11 @@
 import { FooterView } from "@/components/footer/footer-view";
 import { HeaderView } from "@/components/header/header-view";
+import { locales } from "@/configs/i18n";
 import { ThemeProvider } from "@/providers/theme-provider";
+
+export async function generateStaticParams() {
+  return locales.map(locale => ({ locale }));
+}
 
 export default async function IndexLayout({
   children,
