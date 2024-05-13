1ed2e5ccff318d35b7ddc6096d927246d960203e
AndyBe2001 <andybe034@gmail.com>
2024-05-13 16:12:33 +0800

    feat(static page): add static page on root directory

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,14 @@
+import Dictionary from "@/languages/en.json";
+
+export async function generateMetadata() {
+  return {
+    title: `${Dictionary.Common.sitename} - ${Dictionary.About.home.title}`,
+    openGraph: {
+      title: `${Dictionary.Common.sitename} - ${Dictionary.About.home.title}`,
+    },
+  };
+}
+
 export default async function Index() {
   return <></>;
 }
