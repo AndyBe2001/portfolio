18d0d56285c868256188a1822244baa4152a2046
AndyBe2001 <andybe034@gmail.com>
2025-08-11 15:59:05 +0300

    fix(about): fix mobile wierd scroll

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -3,29 +3,12 @@ import { SpeedInsights } from "@vercel/speed-insights/next";
 import { Metadata } from "next";
 import { NextIntlClientProvider } from "next-intl";
 import { getLocale, setRequestLocale } from "next-intl/server";
-import { Noto_Sans_SC, Noto_Sans_TC, Nunito_Sans } from "next/font/google";
 import React from "react";
 
 import { SplashScreen } from "@/components/splash-screen";
 import { routing } from "@/i18n/routing";
 import { ThemeProvider } from "@/providers/theme-provider";
 import { cn } from "@/utils/tailwindcss";
-
-const NunitoSans = Nunito_Sans({
-  subsets: ["latin", "latin-ext"],
-  weight: ["400"],
-  variable: "--font-nunito",
-});
-const NotoSansSC = Noto_Sans_SC({
-  subsets: ["latin", "latin-ext"],
-  weight: ["400"],
-  variable: "--font-noto-sc",
-});
-const NotoSansTC = Noto_Sans_TC({
-  subsets: ["latin", "latin-ext"],
-  weight: ["400"],
-  variable: "--font-noto-tc",
-});
 
 export async function generateMetadata(): Promise<Metadata> {
   return {
@@ -53,7 +36,7 @@ export default async function IndexLayout({
 
   return (
     <html lang={locale} suppressHydrationWarning>
-      <body className={cn(NunitoSans.className, NotoSansSC, "overflow-x-clip")}>
+      <body className={cn("max-w-dvw overflow-x-clip")}>
         <NextIntlClientProvider>
           <ThemeProvider attribute="class">
             <SplashScreen>
