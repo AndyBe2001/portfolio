8feb8728a41f0ca0549c43e4e6febb755fc15092
AndyBe2001 <andybe034@gmail.com>
2025-08-19 08:02:41 +0300

    fix(metadata): openGraph images url

--- src/constants/metadata.ts
+++ src/constants/metadata.ts
@@ -6,7 +6,7 @@ export const DEFAULT_METADATA: Metadata = {
     type: "website",
     images: [
       {
-        url: "https://www.andytsai.com/_next/image?url=%2Fabout%2Fportrait.webp&w=1200&q=75",
+        url: "https://www.andytsai.com/_next/image?url=%2Fopen-graph.jpg&w=1200&q=75",
         width: 1200,
         height: 630,
       },
