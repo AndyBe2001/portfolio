dcd526fbe629b3bcac0d9387bc473c52c76237c9
AndyBe2001 <andybe034@gmail.com>
2025-08-19 07:55:50 +0300

    feature(metadata): default metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,14 +2,30 @@ import { type Metadata } from "next";
 
 import "./global.css";
 
+import { merge } from "lodash";
+import { getTranslations } from "next-intl/server";
 import React from "react";
 
-export const metadata: Metadata = {
-  title: "Andy - Portfolio",
-  openGraph: {
-    title: "Andy - Portfolio",
-    images: ["/portrait.webp"],
-  },
+import { DEFAULT_METADATA } from "@/constants/metadata";
+import { defaultLocale, localeAliases } from "@/i18n/routing";
+
+export const generateMetadata = async (): Promise<Metadata> => {
+  const translation = await getTranslations("pages.home.metadata");
+  const [ogLocale, ...ogAlternateLocale] =
+    localeAliases[defaultLocale as keyof typeof localeAliases];
+
+  return merge({}, DEFAULT_METADATA, {
+    title: translation("title"),
+    description: translation("description"),
+    keywords: translation("keywords"),
+    openGraph: {
+      title: translation("title"),
+      description: translation("description"),
+      url: "https://www.andytsai.com",
+      locale: ogLocale,
+      alternateLocale: ogAlternateLocale,
+    },
+  });
 };
 export default function RootLayout({
   children,
